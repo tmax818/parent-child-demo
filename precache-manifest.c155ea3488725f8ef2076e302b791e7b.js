9498c48e3a45803b0824388298534a15e22321f9
tmax818 <tylermaxwell661@gmail.com>
2020-07-01 13:21:42 -0700

    Updates

---
+++ precache-manifest.c155ea3488725f8ef2076e302b791e7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a08e7136fe3c0c2ff2d231321cdef1f",
+    "url": "/parent-child-demo/index.html"
+  },
+  {
+    "revision": "a81b862bec7ae25a326c",
+    "url": "/parent-child-demo/static/css/main.fe57d6eb.chunk.css"
+  },
+  {
+    "revision": "dcaa5f5a0fc75a6e1e67",
+    "url": "/parent-child-demo/static/js/2.5e572584.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/parent-child-demo/static/js/2.5e572584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a81b862bec7ae25a326c",
+    "url": "/parent-child-demo/static/js/main.8fb59708.chunk.js"
+  },
+  {
+    "revision": "de24fe6485e6971b87bc",
+    "url": "/parent-child-demo/static/js/runtime-main.ae9b4639.js"
+  }
+]);
